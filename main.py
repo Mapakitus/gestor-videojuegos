8bbc3c3da571d102a8741d1576df9ad15ae3f84b
Lueyo <84575656+lueyo@users.noreply.github.com>
2025-11-07 09:40:00 +0100

    gestor de proyecto universalizado y puedes arrancar el servicio ejecutando el main

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-#Ejecutar en la terminal uvicorn main:app --reload
+# Ejecutar en la terminal uvicorn main:app --reload o ejecuta el main y gracias a la sección if __name__ == "__main__" se levantará el servidor automáticamente.
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    import subprocess
+
+    subprocess.run(["uvicorn", "main:app", "--reload"])

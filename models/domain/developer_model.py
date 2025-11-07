dedc7a656905b87ca2d2ed05532c832d944ad298
Lueyo <84575656+lueyo@users.noreply.github.com>
2025-11-07 10:26:02 +0100

    modelos experimentales

---
+++ models/domain/developer_model.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class DeveloperModel(BaseModel):
+    id: str = Field(..., description="Identificador único del desarrollador UUID",) # uuid
+    name: str = Field(..., description="Nombre del desarrollador")
+    founded_date: Optional[str] = Field(None, description="Fecha de fundación del desarrollador en formato YYYY-MM-DD")
+    country: Optional[str] = Field(None, description="País de origen del desarrollador")
+    website: Optional[str] = Field(None, description="Sitio web oficial del desarrollador")
--- models/domain/game_model.py
+++ models/domain/game_model.py
@@ -1,4 +1,13 @@
 # modelo de juego de la aplicacion
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
-#class GameModel(BaseModel):
+
+from models.domain.developer_model import DeveloperModel
+
+class GameModel(BaseModel):
+    id: str = Field(..., description="Identificador único del juego UUID",) # uuid
+    title: str = Field(..., description="Título del juego")
+    date_release: Optional[str] = Field(None, description="Fecha de lanzamiento del juego en formato YYYY-MM-DD")
+    genres: List[str] = Field(default_factory=list, description="Lista de géneros del juego")
+    platform: Optional[str] = Field(None, description="Plataforma del juego ( PC, PS5, Xbox, etc.)")
+    developer: DeveloperModel = Field(..., description="Desarrollador del juego")
